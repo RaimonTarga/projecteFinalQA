491c8e1a1199a278dada900e9c3179cf36496f55
RaimonTarga <raimon.targa.olive@gmail.com>
2024-04-19 11:45:43 +0200

    arreglat el drop down i afegida barra de carrega

--- demo.js
+++ demo.js
@@ -11,6 +11,10 @@ const alturaPokemon = document.getElementById("alturaPokemon")
 const pesoPokemon = document.getElementById("pesoPokemon")
 const imgPokemon = document.getElementById("imgPokemon")
 const cryPokemon = document.getElementById("cryPokemon")
+const pageDropdown = document.getElementById("pageDropdown")
+const ProgressBackGround = document.getElementById("ProgressBackGround")
+const ProgressBar = document.getElementById("ProgressBar")
+
 const banner = document.getElementById("banner")
 
 const PAGE_SIZE = 25
@@ -22,14 +26,20 @@ let filteredResults = []
 
 queryPage(0)
 
+
 searchField.addEventListener('input', (evt) => {
-    clearPage()
     currentPage = 0
     previousButton.disabled = true
     filterElements()
     updateResults()
-    
 })
+
+pageDropdown.addEventListener("change", (event)=> {
+    currentPage = event.target.value-1
+    previousButton.disabled = (currentPage == 0)
+    updateResults(false)
+});
+
 
 function filterElements(){
     filteredResults = []
@@ -38,6 +48,22 @@ function filterElements(){
             filteredResults.push(element)
         }
     })
+}
+
+function updateProgressBar(pagesLoaded){
+    let value = pagesLoaded/(MAX_PAGES-1)
+    var width = value * 100;
+    if (width >= 100) {
+        ProgressBar.style.width = "100%";
+        ProgressBar.innerHTML = "100%";
+        let timer = setTimeout( function(){
+            ProgressBackGround.style = "display:none"
+            ProgressBar.style = "display:none"
+        }, 5000)
+    } else {
+        ProgressBar.style.width = width + "%";
+        ProgressBar.innerHTML = Math.floor(width) + "%";
+    }
 }
 
 function queryPage(pageNumber){
@@ -72,7 +98,11 @@ function queryPage(pageNumber){
                     if (pageNumber==0) updateResults()
                     if (pageNumber<MAX_PAGES){
                         queryPage(pageNumber+1)
-                    }
+                        updateProgressBar(pageNumber)
+                        
+                    } 
+                    clearDropdown()
+                    updateDropdown(pageNumber)
                 })
             })
             .catch(error => {
@@ -135,6 +165,7 @@ function showElement(element){
     
     cryPokemon.onclick = function(){
         let cry = new Audio(element.cries.latest)
+        cry.volume = 0.2
         cry.play()
     }
 }
@@ -145,7 +176,6 @@ function clearPage(){
 
 function nextPage(){
     if (currentPage*PAGE_SIZE+PAGE_SIZE <= queryResults.length){
-        clearPage()
         currentPage++
         updateResults()
         if (currentPage != 0){
@@ -154,6 +184,7 @@ function nextPage(){
         if (currentPage == MAX_PAGES){
             nextButton.disabled = true
         }
+        pageDropdown.selectedIndex = currentPage
     }
 }
 
@@ -165,28 +196,51 @@ function previousPage(){
         previousButton.disabled = true
     }
     nextButton.disabled = false
+    pageDropdown.selectedIndex = currentPage
 }
 
 function goBack(){
-    updateResults()
+    updateResults(false)
     results.style = "display:block"
     banner.style = "display:block"
     fichaPokemon.style = "display:none"
 }
 
-function updateResults(){
+function clearDropdown(){
+    pageDropdown.innerHTML = ""
+}
+
+function updateDropdown(pages){
+    for (let i = 0; i < pages; i++){
+        let option = document.createElement('option')
+        option.text = option.value = i+1
+        pageDropdown.appendChild(option)
+    }
+    pageDropdown.selectedIndex = currentPage
+}
+
+function updateResults(changeDropdown = true){
     clearPage()
     nextButton.disabled = false
     let resultPage = []
+    if (changeDropdown) clearDropdown()
     if (searchField.value != ""){
         resultPage = filteredResults.slice(currentPage*PAGE_SIZE, currentPage*PAGE_SIZE + PAGE_SIZE)
+        if (changeDropdown){
+            updateDropdown(Math.ceil(filteredResults.length/25))
+        }
+        
     }
     else {
         resultPage = queryResults.slice(currentPage*PAGE_SIZE, currentPage*PAGE_SIZE + PAGE_SIZE)
+        if (changeDropdown){
+            updateDropdown(Math.ceil(queryResults.length/25))
+        }
     }
     resultPage.forEach(element => {
         displayElement(element)
     })
+    
     if (resultPage.length < PAGE_SIZE){
         nextButton.disabled = true
     }
